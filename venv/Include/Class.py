807639186e743653bbf2e7253d794c33d7d6626f
Jakub <gudzikoff@gmail.com>
2019-10-27 02:51:29 +0100

    2.4 Cyclone Phrases (challenge)

--- venv/Include/Class.py
+++ venv/Include/Class.py
@@ -1,11 +1,24 @@
-print([x+x+1 for x in [0,1,2,3]])
-print([x%3==0 for x in [3,5,9,8]])
-print([x[3:-1]+x[-1] for x in ["TA_sam", "TA_guido", "student_poohbear", "student_htiek"] if x[0]=="T" and x[1]=="A"])
-print([x[0].upper() for x in ['apple', 'orange', 'pear']])
-print([x for x in ['apple', 'orange', 'pear'] if x[0]=="a"or x[0]=="p"])
-print([[x,x.__len__()] for x in ['apple', 'orange', 'pear']])
+def is_cyclone_phrase(s):
+    len = s.__len__()
+    front = 0
+    back = -1
+    tab = []
+    for x in range(len):
+        if x % 2 == 0:
+            if s[front] != " ":
+                tab.append(s[front])
+            front += 1
+        else:
+            if s[back] != " ":
+                tab.append(s[back])
+            back -= 1
 
-arr={}
-for x in ['apple', 'orange', 'pear']:
-    arr[x]=arr.get(x,x.__len__())
-print(arr)
+    tabSort = sorted(tab)
+    if(tabSort == tab ):
+        return "is_cyclone_phrase('" +s+"') # => True"
+    else:
+        return "is_cyclone_phrase('" +s+"') # => False"
+
+print(is_cyclone_phrase("a  bcb a"))
+print(is_cyclone_phrase("adjourned"))
+print(is_cyclone_phrase("settled"))
