0a4e1ebeb3e2493310a2410881699947e23c2851
Jakub <gudzikoff@gmail.com>
2019-10-27 02:53:08 +0100

    2.7 Polygon Collision

--- venv/Include/Class.py
+++ venv/Include/Class.py
@@ -1,17 +1,44 @@
-def is_triangle_numbers(s):
-    sum = 0
-    for x in range(s.__len__()):
-        sum += ord(s[x]) - 64
-    check = 0;
-    n = 1
-    while check <= sum:
-        check = (n * (n + 1)) / 2
-        n += 1
-        if check == sum:
-            break
-    if check == sum:
-        return s + " is triangle word"
-    else:
-        return s + " is not triangle word"
+def product(x1, y1, x2, y2, x3, y3):
+    a = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)
+    return a
 
-print(is_triangle_numbers("SKY"))
+
+def isBetween(x1, y1, x2, y2, x3, y3):
+    if min(x1, x2) <= 3 and 3 <= max(x1, x2):
+        return True
+    else:
+        return False
+
+
+def polygon_collision(poly1, poly2):
+    s1 = product(poly1[0], poly1[1], poly2[0], poly2[1], poly1[2], poly1[3])
+
+    s2 = product(poly1[0], poly1[1], poly2[2], poly2[3], poly1[2], poly1[3])
+
+    s3 = product(poly2[0], poly2[1], poly1[0], poly1[1], poly2[2], poly2[3])
+
+    s4 = product(poly2[0], poly2[1], poly1[2], poly1[3], poly2[2], poly2[3])
+
+    if ((s1 > 0 and s2 < 0) or (s1 < 0 and s2 > 0)) and ((s3 < 0 and s4 > 0) or (s3 > 0 or s4 < 0)):
+        return True
+    elif s1 == 0 and isBetween(poly1[0], poly1[1], poly1[2], poly1[3], poly2[0], poly2[1]):
+        return True
+    elif s2 == 0 and isBetween(poly1[0], poly1[1], poly1[2], poly1[3], poly2[2], poly2[3]):
+        return True
+    elif s3 == 0 and isBetween(poly2[0], poly2[1], poly2[2], poly2[3], poly1[0], poly1[1]):
+        return True
+    elif s4 == 0 and isBetween(poly2[0], poly2[1], poly2[2], poly2[3], poly1[2], poly1[3]):
+        return True
+    else:
+        return False
+
+
+# collision
+poly1 = (1, 1, 3, 2)
+poly2 = (2, 2, 3, 1)
+print(polygon_collision(poly1, poly2))
+
+# no collision
+poly3 = (1, 3, 2, 2)
+poly4 = (1, 1, 1, 2)
+print(polygon_collision(poly3, poly4))
